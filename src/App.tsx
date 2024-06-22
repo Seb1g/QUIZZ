929f0d694f9482061f2a2830546cd4c8a2f099ff
7ty2ryz3 <iasakov1978@gmail.com>
2024-06-23 01:28:36 +0500

    first verion

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { RenderCategory } from './ParsingData';
+
+const App = () => {
+  return (
+    <div>
+      <h1>My Trivia App</h1>
+      <RenderCategory />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/ParsingData.tsx
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+
+export const RenderCategory = () => {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    const fetchCategory = async () => {
+      try {
+        const response = await fetch(
+          'https://opentdb.com/api_category.php'
+        );
+        const data = await response.json();
+        setCategories(data.trivia_categories);
+        console.log(data.trivia_categories);
+      } catch (error) {
+        console.error('Error fetching data: ', error);
+      }
+    };
+
+    fetchCategory();
+  }, []);
+
+  return (
+    <div>
+      <h1>Categories from API:</h1>
+      <ul>
+        {categories.map((category) => (
+          <li key={category.id}>{category.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
